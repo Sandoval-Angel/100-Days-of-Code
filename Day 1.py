86dbe89e154689a267357ffd2bf78a78ef15d281
Sandoval-Angel <84886723+Sandoval-Angel@users.noreply.github.com>
2021-06-02 22:50:47 -0500

    Working with Variables to Manage Data
    Creating initial submission

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator!\n')
+
+city_name = input('What city did you grow up in?\n')
+pet_name = input("What's your pet's name?\n")
+
+print(f'\nYour band name could be {city_name} {pet_name}!')
